ade5dbcb61a3311b2a784b116c20a111d4d40241
Agustin Marcelo DE LUCA <agustin.deluca@MJGM.GBA.GOB.AR>
2018-11-23 16:35:52 -0300

    Modificados errores de las vistas por listados vacios

--- model/FlightRepository.php
+++ model/FlightRepository.php
@@ -31,7 +31,7 @@ class FlightRepository extends PDORepository {
     public function listFromSearch($fecha, $ciudadOrigen, $ciudadDestino, $paisOrigen, $paisDestino) {
 
         $query = FlightRepository::getInstance()->queryList("SELECT * FROM vuelo WHERE fecha_salida = ? AND capacidad > 0 AND ciudad_origen = ? AND ciudad_destino = ? AND pais_origen = ? AND pais_destino = ? ", array($fecha, $ciudadOrigen, $ciudadDestino, $paisOrigen, $paisDestino));
-
+        $flights = [];
         foreach ($query[0] as $row) {
             $flight = new Flight ( $row['id'], $row['fecha_salida'], $row['fecha_llegada'], $row['capacidad'], $row['ciudad_origen'], $row['ciudad_destino'], $row['pais_origen'], $row['pais_destino'], $row['precio']);
             $flights[]=$flight;
